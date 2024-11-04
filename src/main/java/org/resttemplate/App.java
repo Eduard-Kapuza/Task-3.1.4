c8b2d7e6dec5dd5a276f13643c6d58beff0b18ad
EDUARDKAPUZA <eduardkapuza@gmail.com>
2024-11-04 18:39:58 +0300

    v1

---
+++ src/main/java/org/resttemplate/App.java
@@ -0,0 +1,11 @@
+package org.resttemplate;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+
+
+    }
+}
